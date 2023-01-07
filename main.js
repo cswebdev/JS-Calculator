f299ccbe129789992a92b656d19d8fab7446caee
Chelsea S <csnider91@live.com>
2023-01-07 12:51:23 -0500

    Allalerts are working. User input is stored in empty array. Next goal - get user input to display on calculator screen

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
-// **You are not allowed to use the JavaScript function `eval`**
-
-// **For the following, use the code from Eloquent JavaScript to handle click events:**
-
-// **Rember that JS follows PEMDAS
-
-// **List of variables in Index.HTML**
-// -----------------------------------
-// * .calculator
-// * .calculator-screen
-// * .calculator-keys
-// * <button type="button">
-// * .clear
-// * .plus-minus
-// * .percent
-// * .operator
-// * .number
-// * numers have values attatched 0-9
-// * .number btn-lrg
-// * .decimal
-// * .equal-sign
-
 (function () {
   "use strict";
-  // assigning primary classes a variable and using querySelector to interact with associated HTML
 
+  // Selects elements on document with query selector
   const $calculator = document.querySelector(".calculator");
   const $calScreen = document.querySelector(".calculator-screen");
   const $calKeys = document.querySelectorAll(".calculator-keys");
@@ -32,52 +10,49 @@
   const $calcEquals = document.querySelector(".equal-sign");
   const $clearCalc = document.querySelector(".clear");
 
-  // pushNumber (event)
-
-  // *** | Group Event Listners | ***
-
+  var userInputArr = [];
+  var calcFirstInput = [];
+  var calcSecondInput = [];
+  var calcTotal = [];
+  // Callback functions for numbers/operators
   function pushNumber(event) {
-    alert(event.target.value);
+    const clickedKey = event.target.value;
+    userInputArr.push(clickedKey);
+    console.log(userInputArr);
   }
-
-  $keyValue.forEach(function (button) {
-    //**** Do Not Delete ****/
-    // alert works
-    //
-    button.addEventListener("click", pushNumber);
-    //
-    // alert end
-    // console.log(button);
-    // console.dir(button);
-  });
 
   function pushOperator(event) {
-    alert(event.target.value);
+    const clickedKey = event.target.value;
+    userInputArr.push(clickedKey);
+    console.log(userInputArr);
   }
+
+  // Event listeners for numbers/operators
+  $keyValue.forEach(function (button) {
+    button.addEventListener("click", pushNumber);
+  });
+
   $operatorSym.forEach(function (button) {
     button.addEventListener("click", pushOperator);
   });
 
-  //*** | Individual Event Listners | ***/
-  $calcEquals.addEventListener("click", pushOperator);
+  $calcEquals.addEventListener("click", () => {
+    for (let i = 0; i < userInputArr.length; i++) {
+      console.log(userInputArr[i]);
+    }
+    console.log("We pressed the equal button ");
+  });
   $clearCalc.addEventListener("click", pushOperator);
 
+  //*** | Calculation Section |  ***
   function calculate(event) {
     alert(event.target.button);
   }
 
-  //*** | calculation |  ***/
-  var calculation = [];
-
-  calculation.push(pushNumber());
-  console.log(calculation);
-  console.dir(calculation);
-
-  //
-  //
   //*** | Display |***/
+
   // DO NOT delete
-  // $calScreen = calculation;
+  $calScreen = userInputArr[()];
   console.log("here", $calScreen);
   console.log(typeof $calScreen.value);
   console.log("here", $calKeys.value);

d4b8cc635b5147f1bd01ea8e106d44c238119088
Chelsea S <csnider91@live.com>
2023-01-11 01:13:46 -0500

    major update. Cleaned up code. Removed unused code. Simple caculations work. Decimal and plus/minus button are still unfinished. Simplified object names.

--- scripts/main.js
+++ scripts/main.js
@@ -1,104 +1,100 @@
 (function () {
   "use strict";
 
-  // Selects elements on document with query selector
-  const $calculator = document.querySelector(".calculator");
-  const $calScreen = document.querySelector(".calculator-screen");
-  const $calKeys = document.querySelectorAll(".calculator-keys");
-  const $keyValue = document.querySelectorAll(".number");
-  const $operatorSym = document.querySelectorAll(".operator");
-  const $calcEquals = document.querySelector(".equal-sign");
-  const $clearCalc = document.querySelector(".clear");
+  const $calcDisplay = document.querySelector(".calculator-screen");
+  const $number = document.querySelectorAll(".number");
+  const $operator = document.querySelectorAll(".operator");
+  const $equalSign = document.querySelector(".equal-sign");
+  const $clear = document.querySelector(".clear");
   const $plusMinus = document.querySelector(".plus-minus");
   const $decimal = document.querySelector(".decimal");
 
-  var calculation = [];
-  var calcFirstInput = [];
-  var calcSecondInput = [];
-  // var calcTotal
-  // Callback functions for numbers/operators
+  let calculation = [];
+  // Push number value to calculation array
   function pushNumber(event) {
-    const clickedKey = event.target.value;
-    calculation.push(clickedKey);
-    console.log(calculation);
-    alert(event.target.value);
-    $calScreen.value = calculation.toString().split(",").join("");
+    let value = event.target.value;
+    calculation.push(value);
+    updateDisplay();
+    alert(value);
   }
-
+  // Push operator value to calculation array
   function pushOperator(event) {
-    const clickedKey = event.target.value;
-    calculation.push(clickedKey);
-    console.log(calculation);
-    $calScreen.value = calculation.toString().split(",").join("");
+    let value = event.target.value;
+    calculation.push(value);
+    updateDisplay();
+    alert(value);
+  }
+  // Perform calculation using input in calculation array
+  function calculate() {
+    // Start with first number
+    let result = calculation[0];
+    // loop  through calculation array using the length of the caluclation.
+    for (let i = 1; i < calculation.length; i++) {
+      // Check the operator and apply corresponding operation
+      if (calculation[i] === "+") {
+        result = +result + +calculation[i + 1];
+      } else if (calculation[i] === "-") {
+        result = +result - +calculation[i + 1];
+      } else if (calculation[i] === "*") {
+        result = +result * +calculation[i + 1];
+      } else if (calculation[i] === "/") {
+        result = +result / +calculation[i + 1];
+      }
+    }
+    calculation = [result];
+    updateDisplay();
   }
 
-  function pushEqual(event) {
-    const clickedKey = event.target.value;
-    calculation.push(clickedKey);
-    console.log(calculation);
-    for (let i = 0; i < calculation.length; i++) {
-      console.log(calculation[i]);
-      console.log("The equal button was pressed");
-      alert(event.target.value);
-      $calScreen.value = calculation.toString().split(",").join("");
+  function clear() {
+    calculation = [];
+    updateDisplay();
+  }
+
+  function plusMinus() {
+    // Get the current value of the calculator screen
+
+    let currentOutput = Number($calcDisplay.value);
+
+    // Get the last element entered into the calculation array
+    let lastElement = calculation[calculation.length - 1];
+
+    // Check if last element entered is not empty
+    if (lastElement != "") {
+      // Check if last element entered is a negative number
+      if (lastElement[0] === "-") {
+        // remove the negative sign from the last element
+        lastElement = lastElement.slice(1);
+      } else {
+        // Add a negative sign to the last element
+        lastElement = "-" + lastElement;
+      }
+      // Update the last element in the calculation array with the new value
+      calculation[calculation.length - 1] = lastElement;
+    }
+    // Update the calculator screen with the new value
+    $calcDisplay.value = lastElement;
+  }
+  function decimal() {
+    let lastElement = calculation[calculation.length - 1];
+    if (typeof lastElement === "number" || lastElement.indexOf(".") === -1) {
+      calculation.push(".");
+      updateDisplay();
     }
   }
 
-  function pushPlusMinus() {
-    const currentOutput = Number($calScreen.value);
-    $calScreen.value = -currentOutput;
+  function updateDisplay() {
+    $calcDisplay.value = calculation.join("");
   }
 
-  function addDecimal() {
-    if ($calScreen.value !== "") {
-      if ($calScreen.value.indexOf(".") === -1) {
-        $calScreen.value += ".";
-        $calScreen.value = Number($calScreen.value);
-      }
-    }
-    $calScreen.value = calculation.toString().split(",").join("");
+  for (var i = 0; i < $operator.length; i++) {
+    $operator[i].addEventListener("click", pushOperator);
   }
-  // Event listeners for numbers/operators
-
-  $keyValue.forEach(function (button) {
-    button.addEventListener("click", pushNumber);
-  });
-
-  $operatorSym.forEach(function (button) {
-    button.addEventListener("click", pushOperator);
-  });
-
-  $calcEquals.addEventListener("click", pushEqual);
-
-  $plusMinus.addEventListener("click", pushPlusMinus);
-
-  $decimal.addEventListener("click", addDecimal);
-
-  //*** | Calculation Section |  ***
-
-  function addition(num1, num2) {
-    let sumAnswer = [];
-    for (let i = 0; i < calculation.length; i++)
-      if ($calScreen[i] == $calcEquals.value) {
-        console.log("equal sign is targetted");
-      }
+  for (var i = 0; i < $number.length; i++) {
+    $number[i].addEventListener("click", pushNumber);
   }
+  $equalSign.addEventListener("click", calculate);
 
-  // $calcTotal(function (button) {});
-
-  // *** | Calculation Total ||(SUM)|| **
-
-  // function calcArr(array) {
-  //   if ((pushOperator().matches = [("+", "-", "/", "*")])) {
-  //     console.log("You're adding");
-  //   }
-  // }
-  //*** | Display |***/
-
-  // DO NOT delete
-  // $calScreen = document.querySelector(".calculator-screen");
-  console.log("here", $calScreen);
-  console.log(typeof $calScreen.value);
-  console.log("here", $calKeys.value);
-  console.log(typeof $calKeys);
+  $decimal.addEventListener("click", decimal);
+  $plusMinus.addEventListener("click", plusMinus);
+  $clear.addEventListener("click", clear);
 })();

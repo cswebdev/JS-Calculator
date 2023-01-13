e5b22ff194fbac12f5bcb5fb50d63bdcdb82bcd0
Chelsea S <csnider91@live.com>
2023-01-13 01:38:45 -0500

    bug fixed. Fixed calculation error and faulty logic. No issues with leading or trailing zeros anymore. Calculation array properly clearning now

--- scripts/main.js
+++ scripts/main.js
@@ -2,99 +2,126 @@
   "use strict";
 
   const $calcDisplay = document.querySelector(".calculator-screen");
-  const $number = document.querySelectorAll(".number");
+  const $numbers = document.querySelectorAll(".number");
   const $operator = document.querySelectorAll(".operator");
   const $equalSign = document.querySelector(".equal-sign");
   const $clear = document.querySelector(".clear");
   const $plusMinus = document.querySelector(".plus-minus");
   const $decimal = document.querySelector(".decimal");
 
-  let calculation = [];
-  // Push number value to calculation array
+  // ************************************************* //
+  // Variable List:
+  // targetValue - value should only be used for event targets
+
+  // resultDisplay - calls $calcDisplay
+
+  let calculationArr = [];
+  let finalResult = [];
+  // ************************************************* //
+
+  // push events - calls event to listner
   function pushNumber(event) {
-    let value = event.target.value;
-    calculation.push(value);
-    updateDisplay();
-    alert(value);
+    let targetValue = event.target.value;
+    if ($calcDisplay.value === "0") {
+      $calcDisplay.value = targetValue;
+    } else {
+      $calcDisplay.value += targetValue;
+    }
+    calculationArr.push(targetValue);
   }
-  // Push operator value to calculation array
+
   function pushOperator(event) {
-    let value = event.target.value;
-    calculation.push(value);
-    updateDisplay();
-    alert(value);
+    let targetValue = event.target.value;
+    // alert(targetValue);
+    calculationArr.push(targetValue);
+    $calcDisplay.value += targetValue;
   }
-  // Perform calculation using input in calculation array
+  function equalsSign(event) {
+    calculate();
+    $calcDisplay.value = finalResult;
+    console.log(finalResult);
+  }
+  // ************************************************* //
+  // calculation
+
   function calculate() {
-    // Start with first number
-    let result = calculation[0];
-    // loop  through calculation array using the length of the caluclation.
-    for (let i = 1; i < calculation.length; i++) {
-      // Check the operator and apply corresponding operation
-      if (calculation[i] === "+") {
-        result = +result + +calculation[i + 1];
-      } else if (calculation[i] === "-") {
-        result = +result - +calculation[i + 1];
-      } else if (calculation[i] === "*") {
-        result = +result * +calculation[i + 1];
-      } else if (calculation[i] === "/") {
-        result = +result / +calculation[i + 1];
-      }
-    }
-    calculation = [result];
-    updateDisplay();
-  }
+    let num1 = "",
+      num2 = "",
+      operator = null;
+    const operators = ["+", "-", "*", "/"];
 
-  function clear() {
-    calculation = [];
-    updateDisplay();
-  }
+    for (let i = 0; i < calculationArr.length; i++) {
+      const value = calculationArr[i];
 
-  function plusMinus() {
-    // Get the current value of the calculator screen
-
-    let currentOutput = Number($calcDisplay.value);
-
-    // Get the last element entered into the calculation array
-    let lastElement = calculation[calculation.length - 1];
-
-    // Check if last element entered is not empty
-    if (lastElement != "") {
-      // Check if last element entered is a negative number
-      if (lastElement[0] === "-") {
-        // remove the negative sign from the last element
-        lastElement = lastElement.slice(1);
+      if (operators.includes(value)) {
+        operator = value;
+      } else if (operator == null) {
+        num1 += value;
       } else {
-        // Add a negative sign to the last element
-        lastElement = "-" + lastElement;
+        num2 += value;
       }
-      // Update the last element in the calculation array with the new value
-      calculation[calculation.length - 1] = lastElement;
     }
-    // Update the calculator screen with the new value
-    $calcDisplay.value = lastElement;
-  }
-  function decimal() {
-    let lastElement = calculation[calculation.length - 1];
-    if (typeof lastElement === "number" || lastElement.indexOf(".") === -1) {
-      calculation.push(".");
-      updateDisplay();
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if (operator === "+") {
+      finalResult = num1 + num2;
+      console.log(num1, num2);
+      console.log(finalResult);
+    } else if (operator === "-") {
+      finalResult = num1 - num2;
+    } else if (operator === "*") {
+      finalResult = num1 * num2;
+    } else if (operator === "/") {
+      finalResult = num1 / num2;
     }
+
+    console.log(finalResult);
   }
 
-  function updateDisplay() {
-    $calcDisplay.value = calculation.join("");
+  // ************************************************* //
+  // clear
+  function clear(event) {
+    let targetValue = event.target.value;
+    calculationArr = [];
+    finalResult = [];
+    $calcDisplay.value = "";
+  }
+  function plusMinus(event) {
+    if (typeof finalResult === "number") {
+      finalResult = finalResult * -1;
+      console.log(finalResult);
+    }
+    $calcDisplay.value = finalResult;
   }
 
-  for (var i = 0; i < $operator.length; i++) {
-    $operator[i].addEventListener("click", pushOperator);
+  function decimal(event) {
+    let targetValue = event.target.value;
+    $calcDisplay.value += targetValue;
   }
-  for (var i = 0; i < $number.length; i++) {
-    $number[i].addEventListener("click", pushNumber);
-  }
-  $equalSign.addEventListener("click", calculate);
+  // ************************************************* //
+  // Display
 
+  // displayArray = function display(disp) {
+  //   $calcDisplay = finalResult.toString();
+  // };
+
+  // ************************************************* //
+  //  event listner click events
+
+  $operator.forEach(function (number) {
+    number.addEventListener("click", pushOperator);
+  });
+
+  $numbers.forEach(function (number) {
+    number.addEventListener("click", pushNumber);
+  });
+
+  $equalSign.addEventListener("click", equalsSign);
   $decimal.addEventListener("click", decimal);
   $plusMinus.addEventListener("click", plusMinus);
   $clear.addEventListener("click", clear);
+
+  // ************************************************* //
 })();
